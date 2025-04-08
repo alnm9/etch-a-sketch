da32eeaf3ebce13bce7390c49d9c61a879637ebd
Alina M <189365964+alnm9@users.noreply.github.com>
2025-04-08 16:28:14 +0200

    Add a button to change the grid size

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 const container = document.querySelector(".container");
+const btn = document.querySelector("#btn");
 
-for (let i = 1; i <= 256; i++) {
-    const div = document.createElement("div");
-    div.classList.add("pixel");
-    container.appendChild(div);
-}
+btn.addEventListener("click", () => {
+    let gridNum = prompt("Enter a number for a new grid");
+    for (let i = 1; i <= gridNum ** 2; i++) {
+        const div = document.createElement("div");
+        div.classList.add("pixel");
+        container.appendChild(div);
+        div.style.width = `${100 / gridNum}%`;
+        div.style.height = `${100 / gridNum}%`;
+    }
+})
+
+
+
+
+
+// div.addEventListener("mouseover", (e) => {
+//     e.target.setAttribute("style", "background-color: rgb(157, 203, 230)");
+// })
